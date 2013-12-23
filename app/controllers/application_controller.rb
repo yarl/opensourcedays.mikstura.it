2f60dc3fd336baf39ee4283d241961cddd5a1375
Michal Czyz <michalczyz@gmail.com>
2013-12-23 13:18:36 +0100

    adding language switch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale_based_on_hostname
+
+
+  private
+
+  def set_locale_based_on_hostname
+    I18n.locale = case request.host
+                    when 'dwo.mikstura.it'
+                      :pl
+                    when 'opensourcedays.mikstura.it'
+                      :en
+                    else
+                      I18n.default
+                  end
+  end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+  def i18n_switch_link
+    { en: '//dwo.mikstura.it', pl: '//opensourcedays.mikstura.it'}[I18n.locale]
+  end
+
+  def i18n_switch_flag
+    image_tag("layout/flags/#{{ en: 'pl', pl: 'gb'}[I18n.locale]}.png", style: "height: 16px")
+  end
+
+  def i18n_switch
+    link_to i18n_switch_link do
+      i18n_switch_flag
+    end
+  end
 end
