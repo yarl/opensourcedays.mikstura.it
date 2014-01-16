ccba6219a52fe9eb91efa63392e17c708c5d3fe9
Michal Czyz <michalczyz@gmail.com>
2014-01-16 21:01:56 +0100

    schedule section (still unfinished)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   #after_action :cache_index, only: [:index]
   def index
     prepare_speakers
+    prepare_team
+    prepare_talks
   end
 
   private
@@ -34,5 +36,148 @@ class HomeController < ApplicationController
                      linkedin: 'http://pl.linkedin.com/pub/kate-terlecka/19/738/77a'}
         }
     ].map { |speaker| OpenStruct.new(speaker) }
+  end
+
+  def prepare_team
+    @team = [{
+                 name: 'Dominik Kozaczko', title: 'Wykładowca ATH',
+                 description: 'Ojciec założyciel Dni Wolnego Oprogramowania',
+                 picture: 'Dominik_Kozaczko.jpg',
+                 social: {twitter: 'https://twitter.com/_DomKo_',
+                          google: 'https://plus.google.com/110911336147638586954/posts',
+                          linkedin: 'http://pl.linkedin.com/pub/dominik-kozaczko/30/6a3/844'}
+             },
+             {
+                 name: 'Michał Czyż', title: 'VP @ Mikstura.IT',
+                 description: 'Ojciec założyciel Dni Wolnego Oprogramowania',
+                 picture: 'Michal_Czyz.jpg',
+                 social: {twitter: 'http://bit.ly/twitter_michalczyz',
+                          google: 'http://bit.ly/michalczyz_googleplus',
+                          linkedin: 'http://linkd.in/michalczyz'}
+             }
+    ].map { |teammate| OpenStruct.new(teammate) }
+  end
+
+  def prepare_talks
+    break_time = {
+        type: :break, title: t('schedule.break.header'),
+        speaker: nil,
+        description: t('schedule.break.description')
+    }
+    @talks = {
+        dayone: [
+            {start_at: '15:00', type: :registration, title: t('schedule.registration.header'), speaker: nil,
+             description: t('schedule.registration.description')},
+            {start_at: '16:00', type: :welcome, title: t('schedule.welcome.header'), speaker: 'Dominik Kozaczko',
+             description: t('schedule.welcome.description')},
+            {start_at: '16:30', type: :talk, title: 'Wprowadzenie do świata free & Open Source',
+             speaker: 'Kamil Gałuszka',
+             description: 'Open Source jest czymś co zmieniło i zmienia świat. Chcę pokazać jak Ty drogi uczestniku możesz zaangażować się w świat otwartego kodu budowanego przez społeczność wielu niesamowitych ludzi.'
+            }, {
+                start_at: '16:50', type: :talk, title: 'Jak zacząć: programowanie',
+                speaker: 'Michał Czyż',
+                description: ' '},
+            {
+                start_at: '17:10', type: :talk, title: 'Umiejętności miękkie w zespołach IT',
+                speaker: 'Dariusz Wylon',
+                description: ' '},
+            break_time.merge({
+                                 start_at: '17:30'}),
+            {
+                start_at: '18:00', type: :keynote, title: 'Scrum - czyli jak poddać się zmianom i zrobić swoje',
+                speaker: 'Kate Terlecka',
+                description: 'Scrum to ostatnio bardzo mądre słowo. Ale w Polsce spotkałam dopiero 4 firmy, które na prawdę wiedzą o co w tym chodzi. Chcę wam przybliżyć co to jest Scrum i dlaczego jeśli już go wprowadzamy, to zapominamy o jego sercu.'},
+            break_time.merge({
+                                 start_at: '19:00'
+                             }),
+            {
+                start_at: '19:15', type: :panel, title: 'Panel Dyskusyjny: "Kariera IT"',
+                speaker: '[]',
+                description: ' '}
+        ].map { |talk| OpenStruct.new(talk) },
+        daytwo: [
+            {
+                start_at: '08:30', type: :breakfest, title: t('schedule.breakfest.header'),
+                speaker: nil,
+                description: t('schedule.breakfest.description')
+            },
+            {
+                start_at: '09:45', type: :welcome, title: t('schedule.welcome.header'),
+                speaker: 'Dariusz Wylon',
+                description: t('schedule.welcome.description')},
+            {
+                start_at: '10:00', type: :keynote, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            break_time.merge(start_at: '11:00'),
+            {
+                start_at: '11:30', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            {
+                start_at: '11:50', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            {
+                start_at: '12:10', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            {
+                start_at: '12:30', type: :lunch, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            {
+                start_at: '13:30', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            {
+                start_at: '13:50', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            {
+                start_at: '14:10', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            break_time.merge(start_at: '14:30'),
+            {
+                start_at: '15:00', type: :ligthingtalks, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            break_time.merge(start_at: '16:00'),
+            {
+                start_at: '16:30', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            {
+                start_at: '16:50', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            {
+                start_at: '17:10', type: :talk, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+            break_time.merge(start_at: '17:30'),
+            {
+                start_at: '18:00', type: :keynote, title: 'TBA',
+                speaker: nil,
+                description: 'TBA'
+            },
+        ].map { |talk| OpenStruct.new(talk) },
+        daythree: [
+
+        ].map { |talk| OpenStruct.new(talk) }
+    }
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,15 +1,49 @@
 module HomeHelper
   def i18n_switch_link
-    { en: '//dwo.mikstura.it', pl: '//opensourcedays.mikstura.it'}[I18n.locale]
+    {en: '//dwo.mikstura.it', pl: '//opensourcedays.mikstura.it'}[I18n.locale]
   end
 
   def i18n_switch_flag
-    image_tag("layout/flags/#{{ en: 'pl', pl: 'gb'}[I18n.locale]}.png", style: "height: 16px")
+    image_tag("layout/flags/#{{en: 'pl', pl: 'gb'}[I18n.locale]}.png", style: "height: 16px")
   end
 
   def i18n_switch
     link_to i18n_switch_link do
       i18n_switch_flag
     end
+  end
+
+  def day_is_active(day)
+    today = Date.today
+    if day == :dayone and today == Date.new(2014, 3, 28)
+      'active'
+    elsif day == :daytwo and today == Date.new(2014, 3, 29)
+      'active'
+    elsif day == :daythree and today == Date.new(2014, 3, 30)
+      'active'
+    elsif day == :dayone
+      'active'
+    else
+      nil
+    end
+  end
+
+  def icon_by_type(type)
+    class_name = {
+        registration: 'iconf-ok',
+        keynote: 'iconf-microphone',
+        talk: 'iconf-microphone',
+        panel: 'iconf-dialogue-box',
+        #breakfest: 'iconf-fast_food',
+        breakfest: 'iconf-coffee',
+        ligthingtalks: 'iconf-microphone',
+        break: 'iconf-coffee',
+        #lunch: 'iconf-pizza',
+        lunch: 'iconf-coffee',
+        welcome: 'iconf-sun',
+        party: 'iconf-wine'
+    }[type]
+
+    content_tag(:i, nil, class: class_name)
   end
 end
